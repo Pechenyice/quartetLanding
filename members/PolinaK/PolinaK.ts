e5c9d3e1180e3cd0e7c631beb91dd44278352e4f
pechenyice <german.iskhakov@mail.ru>
2021-11-18 22:05:42 +0300

    added polina K data

---
+++ members/PolinaK/PolinaK.ts
@@ -0,0 +1,10 @@
+import { IKvartetMember, Instrument } from "../../types";
+
+const PolinaK: IKvartetMember = {
+	name: "Polina",
+	preview: "PolinaK.jpg",
+	instrument: Instrument.VIOLIN,
+	about: "Govnina",
+};
+
+export default PolinaK;
