11c5e1b8ba0f5a94dadea95acd4916f0bf9fdb85
pechenyice <german.iskhakov@mail.ru>
2021-11-18 22:05:51 +0300

    added varvara data

---
+++ members/Varvara/Varvara.ts
@@ -0,0 +1,10 @@
+import { IKvartetMember, Instrument } from "../../types";
+
+const Varvara: IKvartetMember = {
+	name: "Polina",
+	preview: "Varvara.jpg",
+	instrument: Instrument.VIOLIN,
+	about: "Govnina",
+};
+
+export default Varvara;
