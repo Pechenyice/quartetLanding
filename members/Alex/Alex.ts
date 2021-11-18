7c013defdc5f3c5013c4d82fbcad03f496e95cca
pechenyice <german.iskhakov@mail.ru>
2021-11-18 22:05:23 +0300

    added alex data

---
+++ members/Alex/Alex.ts
@@ -0,0 +1,10 @@
+import { IKvartetMember, Instrument } from "../../types";
+
+const Alex: IKvartetMember = {
+	name: "Polina",
+	preview: "Alex.jpg",
+	instrument: Instrument.VIOLIN,
+	about: "Govnina",
+};
+
+export default Alex;
