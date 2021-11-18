5e404b7a302e106804954918a7194a662840ce4e
pechenyice <german.iskhakov@mail.ru>
2021-11-18 22:04:33 +0300

    added display component

---
+++ components/KvartetDisplay/KvartetDisplay.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+import { IKvartet, IKvartetMember, KvartetMember } from "../../types";
+
+export default function KvartetDisplay({ kvartet }: IKvartet): JSX.Element {
+	let kvartetPreview = kvartet.map((m, i) => (
+		<Image
+			key={`${m.name}-${i}`}
+			src={`/${m.preview}`}
+			width="200px"
+			height="200px"
+		/>
+	));
+	let kvartetNames = kvartet.map((m) => m.name);
+	let kvartetAbouts = kvartet.map((m) => m.about);
+
+	return (
+		<>
+			<section>{kvartetPreview}</section>
+			<section>{kvartetNames}</section>
+			<section>{kvartetAbouts}</section>
+		</>
+	);
+}
