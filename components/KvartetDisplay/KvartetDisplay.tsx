827f5ee79677cd2502e920168b798fcc982e9937
Pechenyice <german.iskhakov@mail.ru>
2021-11-19 03:49:56 +0300

    updated imports

--- components/KvartetDisplay/KvartetDisplay.tsx
+++ components/KvartetDisplay/KvartetDisplay.tsx
@@ -1,24 +1,19 @@
-import Image from "next/image";
-import React from "react";
-import { IKvartet, IKvartetMember, KvartetMember } from "../../types";
+import Image from 'next/image';
+import React from 'react';
+import { IKvartet } from '../../types/interfaces';
 
 export default function KvartetDisplay({ kvartet }: IKvartet): JSX.Element {
-	let kvartetPreview = kvartet.map((m, i) => (
-		<Image
-			key={`${m.name}-${i}`}
-			src={`/${m.preview}`}
-			width="200px"
-			height="200px"
-		/>
-	));
-	let kvartetNames = kvartet.map((m) => m.name);
-	let kvartetAbouts = kvartet.map((m) => m.about);
+  let kvartetPreview = kvartet.map((m, i) => (
+    <Image key={`${m.name}-${i}`} src={`/${m.preview}`} width="200px" height="200px" />
+  ));
+  let kvartetNames = kvartet.map((m) => m.name);
+  let kvartetAbouts = kvartet.map((m) => m.about);
 
-	return (
-		<>
-			<section>{kvartetPreview}</section>
-			<section>{kvartetNames}</section>
-			<section>{kvartetAbouts}</section>
-		</>
-	);
+  return (
+    <>
+      <section>{kvartetPreview}</section>
+      <section>{kvartetNames}</section>
+      <section>{kvartetAbouts}</section>
+    </>
+  );
 }
