e349a986e0249ebba1279599f139e6f2805991d8
Pechenyice <german.iskhakov@mail.ru>
2021-11-19 03:50:05 +0300

    updated imports

--- members/Varvara/Varvara.ts
+++ members/Varvara/Varvara.ts
@@ -1,10 +1,11 @@
-import { IKvartetMember, Instrument } from "../../types";
+import { Instrument } from '../../types/enums';
+import { IKvartetMember } from '../../types/interfaces';
 
 const Varvara: IKvartetMember = {
-	name: "Polina",
-	preview: "Varvara.jpg",
-	instrument: Instrument.VIOLIN,
-	about: "Govnina",
+  name: 'Polina',
+  preview: 'Varvara.jpg',
+  instrument: Instrument.VIOLIN,
+  about: 'Govnina',
 };
 
 export default Varvara;
