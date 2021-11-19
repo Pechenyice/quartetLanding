398f74c996f6126174e1bf7b4d2dae8a2f05057c
Pechenyice <german.iskhakov@mail.ru>
2021-11-19 03:50:37 +0300

    now index is just a root class for kvartet

--- types/index.ts
+++ types/index.ts
@@ -1,50 +1,36 @@
-export enum Instrument {
-	VIOLIN = 0,
-	CELLO = 1,
-	ALTO = 2,
-}
-
-export interface IKvartetMember {
-	name: string;
-	preview: string;
-	instrument: Instrument;
-	about: string;
-}
-
-export interface IKvartet {
-	kvartet: KvartetMember[];
-}
+import { Instrument } from './enums';
+import { IKvartetMember } from './interfaces';
 
 export class KvartetMember implements IKvartetMember {
-	static serializeMembers = (members: IKvartetMember[]) => {
-		return [...members].map((m) => new KvartetMember(m));
-	};
+  static serializeMembers = (members: IKvartetMember[]) => {
+    return [...members].map((m) => new KvartetMember(m));
+  };
 
-	readonly name: IKvartetMember["name"];
-	readonly preview: IKvartetMember["preview"];
-	readonly instrument: IKvartetMember["instrument"];
-	readonly about: IKvartetMember["about"];
+  readonly name: IKvartetMember['name'];
+  readonly preview: IKvartetMember['preview'];
+  readonly instrument: IKvartetMember['instrument'];
+  readonly about: IKvartetMember['about'];
 
-	constructor(member: IKvartetMember) {
-		this.name = member.name;
-		this.preview = member.preview;
-		this.instrument = member.instrument;
-		this.about = member.about;
-	}
+  constructor(member: IKvartetMember) {
+    this.name = member.name;
+    this.preview = member.preview;
+    this.instrument = member.instrument;
+    this.about = member.about;
+  }
 
-	get getPreview() {
-		return this.preview;
-	}
+  get getPreview() {
+    return this.preview;
+  }
 
-	get getName() {
-		return this.name;
-	}
+  get getName() {
+    return this.name;
+  }
 
-	get getInstrument() {
-		return Instrument[this.instrument];
-	}
+  get getInstrument() {
+    return Instrument[this.instrument];
+  }
 
-	get getAbout() {
-		return this.about;
-	}
+  get getAbout() {
+    return this.about;
+  }
 }
