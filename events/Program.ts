9ab4ddfd3ed60c99d4be303414a0ddf3fb4cac2a
Pechenyice <german.iskhakov@mail.ru>
2021-11-20 22:52:48 +0300

    added data object for program of event

---
+++ events/Program.ts
@@ -0,0 +1,9 @@
+import { IProgram } from '../types/interfaces';
+
+const Program: IProgram = {
+  name: 'Новое выступление',
+  duration: '3 часа',
+  content: ['Биба', 'и', 'Боба'],
+};
+
+export default Program;
