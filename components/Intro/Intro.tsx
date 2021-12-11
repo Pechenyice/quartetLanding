6bb7d40bd4b82c23172d14c2bdbd4dd3596fd27d
Pechenyice <german.iskhakov@mail.ru>
2021-12-11 18:27:45 +0300

    updated Intro component

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import Image from 'next/image';
 import { IKvartet } from '@Types/interfaces';
-import { KvartetDisplay } from '@Components/index';
+import styles from '@Styles/Intro/Intro.module.css';
+import back from '@Public/back.png';
+import violin from '@Public/violin.png';
 
 export default function Intro({ kvartet }: IKvartet): JSX.Element {
   return (
-    <section>
-      <div>
-        test
-        <KvartetDisplay kvartet={kvartet} />
+    <section className={styles.wrapper}>
+      <div className={styles.main}>
+        Clio quartet,
+        <br />
+        который приятно
+        <br />
+        послушать
       </div>
+      <div className={styles.imageBox}>
+        <Image src={back} alt="logo back" />
+        <div className={styles.logo}>
+          <Image src={violin} alt="logo main" />
+        </div>
+      </div>
+      <div className={styles.button}>Заинтересованы?</div>
     </section>
   );
 }
