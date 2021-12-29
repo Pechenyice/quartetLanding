bd77b6baba774c69fbb7818daced54c75a4eef2b
Pechenyice <german.iskhakov@mail.ru>
2021-12-29 20:46:59 +0300

    created display component for kvartet

--- components/KvartetDisplay/KvartetDisplay.tsx
+++ components/KvartetDisplay/KvartetDisplay.tsx
@@ -1,19 +1,65 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect } from 'react';
+import styles from '@Styles/KvartetDisplay/KvartetDisplay.module.css';
+import back from '@Public/back.png';
 import { IKvartet } from '@Types/interfaces';
+import { combineClasses } from '@Utils/index';
 
 export default function KvartetDisplay({ kvartet }: IKvartet): JSX.Element {
-  let kvartetPreview = kvartet.map((m, i) => (
-    <Image key={`${m.name}-${i}`} src={`/${m.preview}`} width="200px" height="200px" />
-  ));
-  let kvartetNames = kvartet.map((m) => m.name);
-  let kvartetAbouts = kvartet.map((m) => m.about);
+  let membersAreas: HTMLCollectionOf<Element> = null as unknown as HTMLCollectionOf<Element>;
 
-  return (
-    <>
-      <section>{kvartetPreview}</section>
-      <section>{kvartetNames}</section>
-      <section>{kvartetAbouts}</section>
-    </>
-  );
+  useEffect(() => {
+    membersAreas = document.getElementsByClassName(styles.memberInner);
+    document.addEventListener('scroll', kvartetDisplayAnimatorController);
+
+    return () => {
+      document.removeEventListener('scroll', kvartetDisplayAnimatorController);
+    };
+  }, []);
+
+  function kvartetDisplayAnimatorController() {
+    for (let memberArea of membersAreas) {
+      if (
+        !memberArea.classList.contains(styles.memberInner_active) &&
+        memberArea.getBoundingClientRect().top < window.innerHeight - 300
+      ) {
+        memberArea.classList.add(styles.memberInner_active);
+        continue;
+      }
+
+      if (
+        memberArea.classList.contains(styles.memberInner_active) &&
+        memberArea.getBoundingClientRect().top > window.innerHeight
+      ) {
+        memberArea.classList.remove(styles.memberInner_active);
+      }
+    }
+  }
+
+  let kvartetPreview = kvartet.map((m, i) => (
+    <section
+      className={combineClasses(styles.memberInner, i % 2 ? styles.memberInner_rightAligned : '')}
+      key={`member-${i}`}
+    >
+      <div className={styles.memberPreview}>
+        <Image
+          src={back}
+          className={styles.memberBack}
+          alt="logo back"
+          layout="fill"
+          objectFit="contain"
+        />
+        <div className={styles.memberImg}>
+          <Image src={`/${m.preview}`} alt="kvartet member" layout="fill" objectFit="contain" />
+        </div>
+      </div>
+      <div className={styles.memberDescription}>
+        <h3 className={styles.memberName}>{m.name}</h3>
+        <h6 className={styles.memberInstrument}>{m.instrument}</h6>
+        <p className={styles.memberAbout}>{m.about}</p>
+      </div>
+    </section>
+  ));
+
+  return <section className={styles.displayWrapper}>{kvartetPreview}</section>;
 }
