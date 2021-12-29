3189dacf94c533923222eb5c1c754db2fa1a8ffc
Pechenyice <german.iskhakov@mail.ru>
2021-12-29 20:49:28 +0300

    updated main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import type { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import styles from '@Styles/Home.module.css';
 import * as members from '@Members/index';
-import { Intro } from '@Components/index';
+import { FloorsDisplay, Intro, KvartetDisplay } from '@Components/index';
 import { KvartetMember } from '@Types/index';
 
 const Home = ({ kvartet }: { kvartet: string }) => {
   let [_kvartet, setKvartet] = useState([] as KvartetMember[]);
+
+  let [floor, setFloor] = useState(0);
+
+  let app = useRef(null as unknown as HTMLElement);
+
   useEffect(() => {
     setKvartet(JSON.parse(kvartet));
   }, [kvartet]);
+
+  useEffect(() => {
+    let sections = Array.from(app.current?.children);
+    sections.reverse();
+    document.addEventListener('scroll', () => {
+      for (let section in sections) {
+        if (sections[section].getBoundingClientRect().top <= 0) {
+          setFloor(sections.length - 1 - +section);
+          return;
+        }
+      }
+    });
+  }, []);
+
   return (
-    <section className={styles.app}>
+    <section className={styles.app} ref={app}>
+      <FloorsDisplay floor={floor} />
       <Intro kvartet={_kvartet} />
+      <KvartetDisplay kvartet={_kvartet} />
     </section>
   );
 };
