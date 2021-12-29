ff15dced59afbf5b445e5bf53decd3fd7d723851
Pechenyice <german.iskhakov@mail.ru>
2021-12-29 20:49:45 +0300

    updated deploy settings

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
+let config = {
   reactStrictMode: true,
-  basePath: '/quartetLanding',
-  assetPrefix: '/quartetLanding',
-  images: {
-    loader: 'imgix',
-    path: 'https://pechenyice.github.io/',
-  },
 };
+
+const isProd = process.env.NODE_ENV.toLowerCase() === 'production';
+
+if (isProd)
+  config = {
+    ...config,
+    images: {
+      loader: 'imgix',
+      path: 'https://Pechenyice.github.io/',
+    },
+    basePath: '/quartetLanding',
+    assetPrefix: '/quartetLanding',
+  };
+
+module.exports = config;
