7ac726ac6b57bd5289cc99829a1def9ee186fc1f
Pechenyice <german.iskhakov@mail.ru>
2021-12-29 20:47:55 +0300

    updated members data

--- members/Varvara/Varvara.ts
+++ members/Varvara/Varvara.ts
@@ -2,10 +2,11 @@ import { Instrument } from '@Types/enums';
 import { IKvartetMember } from '@Types/interfaces';
 
 const Varvara: IKvartetMember = {
-  name: 'Varvara',
-  preview: 'Varvara.jpg',
-  instrument: Instrument.VIOLIN,
-  about: 'Varvara',
+  name: 'Варвара',
+  preview: 'Varvara.png',
+  instrument: Instrument.CELLO,
+  about:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 };
 
 export default Varvara;
