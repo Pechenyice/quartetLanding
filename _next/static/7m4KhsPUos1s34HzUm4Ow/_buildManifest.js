3ec62f7eb0794ff3da9aa1335f6f7d5ec8be8439
Pechenyice <german.iskhakov@mail.ru>
2021-12-29 21:18:32 +0300

    Updates

--- _next/static/7m4KhsPUos1s34HzUm4Ow/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0ac2093deb019c15.css","static/chunks/pages/index-c79853f5a77cfb4a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
