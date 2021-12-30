2390c722d42ca5726c087758f298cdec318d32ea
Pechenyice <german.iskhakov@mail.ru>
2021-12-30 20:34:08 +0300

    updated kvartet display component layout

--- components/KvartetDisplay/KvartetDisplay.tsx
+++ components/KvartetDisplay/KvartetDisplay.tsx
@@ -1,15 +1,20 @@
 import Image from 'next/image';
-import React, { useEffect } from 'react';
+import React, { CSSProperties, useEffect } from 'react';
 import styles from '@Styles/KvartetDisplay/KvartetDisplay.module.css';
 import back from '@Public/back.png';
 import { IKvartet } from '@Types/interfaces';
-import { combineClasses, getPrefix } from '@Utils/index';
+import { combineClasses, withAssetPrefix } from '@Utils/index';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import { quotes } from 'quotes';
 
 export default function KvartetDisplay({ kvartet }: IKvartet): JSX.Element {
   let membersAreas: HTMLCollectionOf<Element> = null as unknown as HTMLCollectionOf<Element>;
+  let quotesArea: Element = null as unknown as Element;
 
   useEffect(() => {
     membersAreas = document.getElementsByClassName(styles.memberInner);
+    quotesArea = document.getElementsByClassName(styles.quotes)[0];
     document.addEventListener('scroll', kvartetDisplayAnimatorController);
 
     return () => {
@@ -18,10 +23,22 @@ export default function KvartetDisplay({ kvartet }: IKvartet): JSX.Element {
   }, []);
 
   function kvartetDisplayAnimatorController() {
+    if (
+      !quotesArea.classList.contains(styles.quotes_active) &&
+      quotesArea.getBoundingClientRect().top < window.innerHeight - 200
+    ) {
+      quotesArea.classList.add(styles.quotes_active);
+    } else if (
+      quotesArea.classList.contains(styles.quotes_active) &&
+      quotesArea.getBoundingClientRect().top > window.innerHeight
+    ) {
+      quotesArea.classList.remove(styles.quotes_active);
+    }
+
     for (let memberArea of membersAreas) {
       if (
         !memberArea.classList.contains(styles.memberInner_active) &&
-        memberArea.getBoundingClientRect().top < window.innerHeight - 300
+        memberArea.getBoundingClientRect().top < window.innerHeight - 200
       ) {
         memberArea.classList.add(styles.memberInner_active);
         continue;
@@ -45,26 +62,59 @@ export default function KvartetDisplay({ kvartet }: IKvartet): JSX.Element {
         <Image
           src={back}
           className={styles.memberBack}
-          alt="logo back"
+          alt="member back"
           layout="fill"
           objectFit="contain"
         />
         <div className={styles.memberImg}>
           <Image
-            src={`${getPrefix()}${m.preview}`}
+            src={`${withAssetPrefix(m.preview)}`}
             alt="kvartet member"
             layout="fill"
             objectFit="contain"
           />
+          <h4 className={styles.memberImageName}>{m.name}</h4>
         </div>
       </div>
       <div className={styles.memberDescription}>
-        <h3 className={styles.memberName}>{m.name}</h3>
-        <h6 className={styles.memberInstrument}>{m.instrument}</h6>
-        <p className={styles.memberAbout}>{m.about}</p>
+        <div className={styles.memberDescriptionImage}>
+          {/* <Image src={back} alt="member back" layout="fill" objectFit="contain" /> */}
+        </div>
+        <div className={styles.memberDescriptionText}>
+          {/* <h3 className={styles.memberName}>{m.name}</h3> */}
+          {/* <h6 className={styles.memberInstrument}>{m.instrument}</h6> */}
+          <p className={styles.memberAbout}>{m.about}</p>
+        </div>
       </div>
     </section>
   ));
 
-  return <section className={styles.displayWrapper}>{kvartetPreview}</section>;
+  let quotesPreview = quotes.map((q, i) => (
+    <div
+      className={styles.quote}
+      key={`quote-${i}`}
+      style={{ '--quote-index': `${(i + 1) * 0.2}` } as CSSProperties}
+    >
+      <div className={styles.quoteContent}>
+        <FontAwesomeIcon
+          className={combineClasses(styles.quoteDecorator, styles.quoteLeft)}
+          icon={faQuoteLeft}
+        />
+        <p className={styles.quoteContent}>{q.content}</p>
+        <FontAwesomeIcon
+          className={combineClasses(styles.quoteDecorator, styles.quoteRight)}
+          icon={faQuoteLeft}
+        />
+      </div>
+      <div className={styles.quoteAuthor}>- {q.author}</div>
+    </div>
+  ));
+
+  return (
+    <section className={styles.displayWrapper}>
+      {kvartetPreview.slice(0, 2)}
+      <div className={styles.quotes}>{quotesPreview}</div>
+      {kvartetPreview.slice(2)}
+    </section>
+  );
 }
