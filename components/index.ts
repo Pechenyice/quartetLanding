40d5a1fd73dcaea4b467c4403797049014bf42da
Pechenyice <german.iskhakov@mail.ru>
2021-12-31 16:43:31 +0300

    indexedquotes

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { default as KvartetDisplay } from './KvartetDisplay/KvartetDisplay';
 export { default as Intro } from './Intro/Intro';
 export { default as FloorsDisplay } from './FloorsDisplay/FloorsDisplay';
+export { default as QuotesDisplay } from './QuotesDisplay/QuotesDisplay';
