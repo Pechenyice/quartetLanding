b9687efbb3d4d54522b81efa029b06d616e96981
Pechenyice <german.iskhakov@mail.ru>
2021-12-31 22:14:15 +0300

    updated components index

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export { default as KvartetDisplay } from './KvartetDisplay/KvartetDisplay';
 export { default as Intro } from './Intro/Intro';
 export { default as FloorsDisplay } from './FloorsDisplay/FloorsDisplay';
 export { default as QuotesDisplay } from './QuotesDisplay/QuotesDisplay';
+export { default as Featuring } from './Featuring/Featuring';
