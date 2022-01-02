29c7401b0a123327a6318cf8187b67516e48d547
Pechenyice <german.iskhakov@mail.ru>
2022-01-02 22:25:48 +0300

    added gallery to index

--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,4 @@ export { default as Intro } from './Intro/Intro';
 export { default as FloorsDisplay } from './FloorsDisplay/FloorsDisplay';
 export { default as QuotesDisplay } from './QuotesDisplay/QuotesDisplay';
 export { default as Featuring } from './Featuring/Featuring';
+export { default as Gallery } from './Gallery/Gallery';
