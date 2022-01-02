77f4e411f6a4b2601dac1fa0e4dfa2639d5d6fcb
Pechenyice <german.iskhakov@mail.ru>
2022-01-02 22:25:57 +0300

    created gallery section

---
+++ components/Gallery/Gallery.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '@Styles/Gallery/Gallery.module.css';
+
+const Gallery = () => {
+  return (
+    <section className={styles.wrapper}>
+      <div className={styles.main}></div>
+      <div></div>
+    </section>
+  );
+};
+
+export default Gallery;
