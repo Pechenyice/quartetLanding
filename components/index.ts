209d213a5c6d711292f8ceea139c7d1b7519ec58
Pechenyice <german.iskhakov@mail.ru>
2022-01-04 20:18:00 +0300

    updated components index

--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,5 @@ export { default as FloorsDisplay } from './FloorsDisplay/FloorsDisplay';
 export { default as QuotesDisplay } from './QuotesDisplay/QuotesDisplay';
 export { default as Featuring } from './Featuring/Featuring';
 export { default as Gallery } from './Gallery/Gallery';
+export { default as Media } from './Media/Media';
+export { default as Footer } from './Footer/Footer';
