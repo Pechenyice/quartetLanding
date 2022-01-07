8c9f8e58168bdc26b69f0d13e1d48f11d8d8081c
Pechenyice <german.iskhakov@mail.ru>
2022-01-07 04:54:36 +0300

    startd Quotes section component

---
+++ components/Alternate/Quotes/Quotes.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Image from 'next/Image';
+import styles from '@Styles/Alternate/Main/Main.module.css';
+import { combineClasses } from '@Utils';
+import ellipse from '@Public/mainPageEllipse.png';
+import star from '@Public/star.png';
+import violin from '@Public/violin.png';
+
+const Quotes = () => {
+  return (
+    <section className={combineClasses(styles.wrapper, 'appScreen')}>
+      <h2 className={styles.heading}>
+        <span>test</span>
+        <span> test </span>
+        <span>test</span>
+      </h2>
+      <div className={styles.images}>
+        <div className={styles.ellipse}>
+          <Image src={ellipse} layout="fill" objectFit="contain" />
+        </div>
+        <div className={combineClasses(styles.star, styles.star_left)}>
+          <Image src={star} layout="fill" objectFit="contain" />
+        </div>
+        <div className={combineClasses(styles.star, styles.star_rightBig)}>
+          <Image src={star} layout="fill" objectFit="contain" />
+        </div>
+        <div className={combineClasses(styles.star, styles.star_rightSmall)}>
+          <Image src={star} layout="fill" objectFit="contain" />
+        </div>
+        <div className={styles.violin}>
+          <Image src={violin} layout="fill" objectFit="contain" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Quotes;
