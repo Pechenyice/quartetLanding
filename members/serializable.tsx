6ca30014863706f099cea3a33461426e984a8aa8
Pechenyice <german.iskhakov@mail.ru>
2022-01-09 03:27:43 +0300

    created members serializable content

---
+++ members/serializable.tsx
@@ -0,0 +1,4 @@
+export { default as Polina } from './Polina/Polina';
+export { default as PolinaK } from './PolinaK/PolinaK';
+export { default as Alex } from './Alex/Alex';
+export { default as Varvara } from './Varvara/Varvara';
