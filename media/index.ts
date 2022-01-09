c9ce3d3e5c2ecc3cd52b7f18aa651f8df33a544e
Pechenyice <german.iskhakov@mail.ru>
2022-01-10 00:17:36 +0300

    updated media

--- media/index.ts
+++ media/index.ts
@@ -9,26 +9,31 @@ import {
 
 const media: IMedia[] = [
   {
+    name: 'Instagram',
     icon: faInstagram,
     link: '#',
     activeColor: '#CD486B',
   },
   {
+    name: 'Facebook',
     icon: faFacebook,
     link: '#',
     activeColor: '#4267B2',
   },
   {
+    name: 'VK',
     icon: faVk,
     link: '#',
     activeColor: '#4C75A3',
   },
   {
+    name: 'Telegram',
     icon: faTelegram,
     link: '#',
     activeColor: '#0088CC',
   },
   {
+    name: 'Whatsapp',
     icon: faWhatsapp,
     link: '#',
     activeColor: '#25D366',
