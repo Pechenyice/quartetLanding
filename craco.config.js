bdcc5f5cf576ff87d10307b7191e649e271e7d27
pechenyice <german.iskhakov@mail.ru>
2022-01-09 20:30:44 +0300

    added aliases

--- craco.config.js
+++ craco.config.js
@@ -1,23 +1,23 @@
 const path = require(`path`);
 
 module.exports = {
-  webpack: {
-    alias: {
-      '@': path.resolve(__dirname, '/'),
-      '@Pages': path.resolve(__dirname, 'pages'),
-      '@Components': path.resolve(__dirname, 'components'),
-      '@Events': path.resolve(__dirname, 'events'),
-      '@Members': path.resolve(__dirname, 'members'),
-      '@Styles': path.resolve(__dirname, 'styles'),
-      '@Types': path.resolve(__dirname, 'types'),
-      '@Public': path.resolve(__dirname, 'public'),
-      '@Utils': path.resolve(__dirname, 'utils'),
-      '@Screens': path.resolve(__dirname, 'screens'),
-      '@HOC': path.resolve(__dirname, 'HOC'),
-      '@Members': path.resolve(__dirname, 'members'),
-
-      '@Quotes': path.resolve(__dirname, 'quotes'),
-      '@Media': path.resolve(__dirname, 'media'),
-    },
-  },
+	webpack: {
+		alias: {
+			"@": path.resolve(__dirname, "/"),
+			"@Pages": path.resolve(__dirname, "pages"),
+			"@Components": path.resolve(__dirname, "components"),
+			"@Events": path.resolve(__dirname, "events"),
+			"@Members": path.resolve(__dirname, "members"),
+			"@Styles": path.resolve(__dirname, "styles"),
+			"@Types": path.resolve(__dirname, "types"),
+			"@Public": path.resolve(__dirname, "public"),
+			"@Utils": path.resolve(__dirname, "utils"),
+			"@Screens": path.resolve(__dirname, "screens"),
+			"@HOC": path.resolve(__dirname, "HOC"),
+			"@Members": path.resolve(__dirname, "members"),
+			"@Decorators": path.resolve(__dirname, "decorators"),
+			"@Quotes": path.resolve(__dirname, "quotes"),
+			"@Media": path.resolve(__dirname, "media"),
+		},
+	},
 };
