a75dd0ba0cd81b78c70c2f8fc8359c93b67a29f0
Pechenyice <german.iskhakov@mail.ru>
2022-01-09 03:28:42 +0300

    updated HOC logic with single options object

--- HOC/index.tsx
+++ HOC/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { IKvartetMember, IMemberScreenProps, IScreenProps } from '@Types/interfaces';
+import {
+  IKvartetMember,
+  IMemberScreenOptions,
+  IMemberScreenProps,
+  IScreenProps,
+} from '@Types/interfaces';
 
 const withMemberData = (
   Component: (props: IMemberScreenProps) => JSX.Element,
-  member: IKvartetMember
+  member: IKvartetMember,
+  options: IMemberScreenOptions
 ) => {
-  const brainedComponent = (isActive: IScreenProps) => <Component member={member} isActive />;
+  const brainedComponent = ({ isActive }: IScreenProps) => (
+    <Component member={member} isActive={isActive} options={options} />
+  );
 
   return brainedComponent;
 };
