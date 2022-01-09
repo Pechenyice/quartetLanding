a8bd6d754dfb8f9040ddedc840f7498a5aff9589
pechenyice <german.iskhakov@mail.ru>
2022-01-09 20:33:48 +0300

    added interfaces for screens

--- types/interfaces.ts
+++ types/interfaces.ts
@@ -1,79 +1,85 @@
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import React from 'react';
-import { KvartetMember } from '.';
-import { Instrument, Navigation } from './enums';
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import React from "react";
+import { KvartetMember } from ".";
+import { Instrument, Navigation } from "./enums";
 
 export interface IKvartetMember {
-  name: string;
-  preview: string;
-  instrument: Instrument;
-  about: string;
+	name: string;
+	preview: string;
+	instrument: Instrument;
+	about: string;
 }
 
-export interface IMemberDecorator {
-  position: {
-    top?: number;
-    left?: number;
-    right?: number;
-    bottom?: number;
-  };
-  width: number;
-  height: number;
-  color: string;
-  opacity?: number;
+export interface IDecorator {
+	position: {
+		top?: number;
+		left?: number;
+		right?: number;
+		bottom?: number;
+	};
+	width: number;
+	height: number;
+	color: string;
+	opacity?: number;
 }
 
 export interface IKvartet {
-  kvartet: KvartetMember[];
+	kvartet: KvartetMember[];
 }
 
 export interface IInformation {
-  name: string;
-  location: string;
-  date: string;
-  price: number;
+	name: string;
+	location: string;
+	date: string;
+	price: number;
 }
 
 export interface IProgram {
-  name: string;
-  duration: string;
-  content: string[];
+	name: string;
+	duration: string;
+	content: string[];
 }
 
 export interface IQuote {
-  content: string;
-  author: string;
+	content: string;
+	author: string;
 }
 
 export interface IMedia {
-  icon: IconDefinition;
-  link: string;
-  activeColor: string;
+	icon: IconDefinition;
+	link: string;
+	activeColor: string;
 }
 
 export interface IScreenManager {
-  activeScreen: number;
-  nextScreen: number | null;
-  isSwapping: boolean;
-  animationDuration: number;
-  sibling: ReturnType<typeof setTimeout> | null;
+	activeScreen: number;
+	nextScreen: number | null;
+	isSwapping: boolean;
+	animationDuration: number;
+	sibling: ReturnType<typeof setTimeout> | null;
 }
 
 export interface IScreenProps {
-  isActive: boolean;
+	isActive: boolean;
 }
 
-export interface IMemberScreenProps extends IScreenProps {
-  member: IKvartetMember;
-  options: IMemberScreenOptions;
+export interface IScreenWithOptionsProps extends IScreenProps {
+	options: IScreenOptions | IMemberScreenOptions;
 }
 
-export interface IMemberScreenOptions {
-  reversed: boolean;
-  decorators: IMemberDecorator[];
+export interface IMemberScreenProps extends IScreenWithOptionsProps {
+	member: IKvartetMember;
+}
+
+export interface IScreenOptions {
+	decorators: IDecorator[];
+}
+
+export interface IMemberScreenOptions extends IScreenOptions {
+	reversed: boolean;
 }
 
 export interface IScreen {
-  component: (props: IScreenProps) => JSX.Element;
-  navigation: Navigation;
+	component: (props: IScreenProps) => JSX.Element;
+	navigation: Navigation;
 }
