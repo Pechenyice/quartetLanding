2705e2c32896e474198b3d902298d97d3bc88344
Pechenyice <german.iskhakov@mail.ru>
2022-01-09 03:28:00 +0300

    created members decorators configs

---
+++ members/Polina/PolinaDecorators.ts
@@ -0,0 +1,33 @@
+import { IMemberDecorator } from '@Types/interfaces';
+
+const circles: IMemberDecorator[] = [
+  {
+    position: {
+      bottom: 190,
+      left: 305,
+    },
+    width: 312,
+    height: 312,
+    color: '#522EBA',
+  },
+  {
+    position: {
+      bottom: -192,
+      left: -192,
+    },
+    width: 568,
+    height: 568,
+    color: '#7B6866',
+  },
+  {
+    position: {
+      left: 505,
+      bottom: 212,
+    },
+    width: 460,
+    height: 460,
+    color: '#CD794A',
+  },
+];
+
+export default circles;
---
+++ members/Varvara/VarvaraDecorators.tsx
@@ -0,0 +1,33 @@
+import { IMemberDecorator } from '@Types/interfaces';
+
+const circles: IMemberDecorator[] = [
+  {
+    position: {
+      bottom: 190,
+      right: 305,
+    },
+    width: 312,
+    height: 312,
+    color: '#1D7C0D',
+  },
+  {
+    position: {
+      bottom: -183,
+      left: -157,
+    },
+    width: 550,
+    height: 550,
+    color: '#5F5033',
+  },
+  {
+    position: {
+      right: 505,
+      bottom: 212,
+    },
+    width: 461,
+    height: 461,
+    color: '#C46129',
+  },
+];
+
+export default circles;
