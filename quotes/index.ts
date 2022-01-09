e5d60190caafbcfe678489125b41e5092015d6a3
pechenyice <german.iskhakov@mail.ru>
2022-01-09 20:31:03 +0300

    updates quotes

--- quotes/index.ts
+++ quotes/index.ts
@@ -1,30 +1,22 @@
-import { IQuote } from '@Types/interfaces';
+import { IQuote } from "@Types/interfaces";
 
 let quotes: IQuote[] = [
-  {
-    content: '10 / 10',
-    author: 'Гена',
-  },
-  {
-    content: 'Это просто невероятно',
-    author: 'Биба',
-  },
-  {
-    content: 'Ошеломительно',
-    author: 'Боба',
-  },
-  {
-    content: '10 / 10',
-    author: 'Гена',
-  },
-  {
-    content: 'Это просто невероятно',
-    author: 'Биба',
-  },
-  {
-    content: 'Ошеломительно',
-    author: 'Боба',
-  },
+	{
+		content: "10 / 10",
+		author: "Исхакова П.Т.",
+	},
+	{
+		content: "Такое",
+		author: "Исхаков Г.Т.",
+	},
+	{
+		content: "Приятно послушать",
+		author: "Исхакова П.Т.",
+	},
+	{
+		content: "Полина просто великолепна",
+		author: "Исхакова П.Т.",
+	},
 ];
 
 export { quotes };
