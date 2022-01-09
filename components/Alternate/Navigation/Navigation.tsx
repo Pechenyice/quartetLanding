92d0b80b5bc84316a49e3e338c9c67c3d43d97da
Pechenyice <german.iskhakov@mail.ru>
2022-01-10 00:16:57 +0300

    added navigation component

--- components/Alternate/Navigation/Navigation.tsx
+++ components/Alternate/Navigation/Navigation.tsx
@@ -3,7 +3,15 @@ import styles from '@Styles/Alternate/Navigation/Navigation.module.css';
 import { Navigation as navigation } from '@Types/enums';
 import { combineClasses } from '@Utils';
 
-const Navigation = ({ activePoint }: { activePoint: navigation }) => {
+const Navigation = ({
+  activePoint,
+  clickManager,
+}: {
+  activePoint: navigation;
+  clickManager: (index: number) => void;
+}) => {
+  let bindClick = (i: number) => () => clickManager(i);
+
   return (
     <section className={combineClasses(styles.wrapper, +activePoint ? styles.colored : '')}>
       {Object.keys(navigation)
@@ -14,7 +22,9 @@ const Navigation = ({ activePoint }: { activePoint: navigation }) => {
             className={combineClasses(styles.nav, +activePoint === +value ? styles.nav_active : '')}
           >
             <div>
-              <div className={styles.number}>{`0${+value + 1}`.slice(-2)}</div>
+              <div className={styles.number} onClick={bindClick(+i)}>
+                {`0${+value + 1}`.slice(-2)}
+              </div>
               <div className={styles.name}>{navigation[+value]}</div>
             </div>
           </div>
