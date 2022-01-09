6bca5243dd1d67ad55d3e519c0fffe3e3ff586cc
pechenyice <german.iskhakov@mail.ru>
2022-01-09 20:30:25 +0300

    added HOC for binding decorators to screen

--- HOC/index.tsx
+++ HOC/index.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React from "react";
 import {
-  IKvartetMember,
-  IMemberScreenOptions,
-  IMemberScreenProps,
-  IScreenProps,
-} from '@Types/interfaces';
+	IKvartetMember,
+	IMemberScreenOptions,
+	IMemberScreenProps,
+	IScreenProps,
+	IScreenOptions,
+	IScreenWithOptionsProps,
+} from "@Types/interfaces";
 
 const withMemberData = (
-  Component: (props: IMemberScreenProps) => JSX.Element,
-  member: IKvartetMember,
-  options: IMemberScreenOptions
+	Component: (props: IMemberScreenProps) => JSX.Element,
+	member: IKvartetMember,
+	options: IMemberScreenOptions
 ) => {
-  const brainedComponent = ({ isActive }: IScreenProps) => (
-    <Component member={member} isActive={isActive} options={options} />
-  );
+	const brainedComponent = ({ isActive }: IScreenProps) => (
+		<Component member={member} isActive={isActive} options={options} />
+	);
 
-  return brainedComponent;
+	return brainedComponent;
 };
 
-export { withMemberData };
+const withOptions = (
+	Component: (props: IScreenWithOptionsProps) => JSX.Element,
+	options: IScreenOptions
+) => {
+	const decoratedComponent = ({ isActive }: IScreenProps) => (
+		<Component isActive={isActive} options={options as IScreenOptions} />
+	);
+
+	return decoratedComponent;
+};
+
+export { withMemberData, withOptions };
