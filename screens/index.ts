bb2a696811f0e34be47c10adb4861e981711c139
Pechenyice <german.iskhakov@mail.ru>
2022-01-10 00:18:03 +0300

    updated screens config

--- screens/index.ts
+++ screens/index.ts
@@ -1,58 +1,66 @@
-import { Events, Main, Member, Quotes } from "@Components";
-import { Alex, Polina, PolinaK, Varvara } from "@Members";
+import { Archive, CustomMedia, Events, Main, Member, Quotes } from '@Components';
+import { Alex, Polina, PolinaK, Varvara } from '@Members';
 import {
-	PolinaDecorators,
-	PolinaKDecorators,
-	VarvaraDecorators,
-	AlexDecorators,
-} from "@Members/decorators";
-import { Navigation } from "@Types/enums";
-import { IScreen } from "@Types/interfaces";
-import { eventsDecorators, quotesDecorators } from "@Decorators";
-import { withMemberData, withOptions } from "HOC";
-import { formatWithOptions } from "util";
+  PolinaDecorators,
+  PolinaKDecorators,
+  VarvaraDecorators,
+  AlexDecorators,
+} from '@Members/decorators';
+import { Navigation } from '@Types/enums';
+import { IScreen } from '@Types/interfaces';
+import { archiveDecorators, eventsDecorators, quotesDecorators } from '@Decorators';
+import { withMemberData, withOptions } from 'HOC';
+import { formatWithOptions } from 'util';
 
 const screens: IScreen[] = [
-	{
-		component: Main,
-		navigation: Navigation["Clio quartet"],
-	},
-	{
-		component: withMemberData(Member, Polina, {
-			reversed: false,
-			decorators: PolinaDecorators,
-		}),
-		navigation: Navigation["Наша команда"],
-	},
-	{
-		component: withMemberData(Member, Alex, {
-			reversed: true,
-			decorators: AlexDecorators,
-		}),
-		navigation: Navigation["Наша команда"],
-	},
-	{
-		component: withMemberData(Member, PolinaK, {
-			reversed: false,
-			decorators: PolinaKDecorators,
-		}),
-		navigation: Navigation["Наша команда"],
-	},
-	{
-		component: withMemberData(Member, Varvara, {
-			reversed: true,
-			decorators: VarvaraDecorators,
-		}),
-		navigation: Navigation["Наша команда"],
-	},
-	{
-		component: withOptions(Quotes, { decorators: quotesDecorators }),
-		navigation: Navigation["О нас"],
-	},
-	{
-		component: withOptions(Events, { decorators: eventsDecorators }),
-		navigation: Navigation["Выступления"],
-	},
+  {
+    component: Main,
+    navigation: Navigation['Clio quartet'],
+  },
+  {
+    component: withMemberData(Member, Polina, {
+      reversed: false,
+      decorators: PolinaDecorators,
+    }),
+    navigation: Navigation['Наша команда / Полина'],
+  },
+  {
+    component: withMemberData(Member, Alex, {
+      reversed: true,
+      decorators: AlexDecorators,
+    }),
+    navigation: Navigation['Наша команда / Александра'],
+  },
+  {
+    component: withMemberData(Member, PolinaK, {
+      reversed: false,
+      decorators: PolinaKDecorators,
+    }),
+    navigation: Navigation['Наша команда / Пoлина'],
+  },
+  {
+    component: withMemberData(Member, Varvara, {
+      reversed: true,
+      decorators: VarvaraDecorators,
+    }),
+    navigation: Navigation['Наша команда / Варвара'],
+  },
+  {
+    component: withOptions(Quotes, { decorators: quotesDecorators }),
+    navigation: Navigation['О нас'],
+  },
+  {
+    component: withOptions(Events, { decorators: eventsDecorators }),
+    navigation: Navigation['Выступления'],
+  },
+  {
+    component: withOptions(Archive, { decorators: archiveDecorators }),
+    navigation: Navigation['Архив'],
+  },
+  {
+    component: CustomMedia,
+    navigation: Navigation['Наши медиа'],
+  },
 ];
 
 const getScreen = (index: number) => screens[index];
