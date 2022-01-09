c8f79541ef7615bab6f4773a8dd5e21d51ddf5f9
Pechenyice <german.iskhakov@mail.ru>
2022-01-09 03:27:30 +0300

    updated members index

--- members/index.ts
+++ members/index.ts
@@ -1,4 +1,8 @@
-export { default as Polina } from "./Polina/Polina";
-export { default as PolinaK } from "./PolinaK/PolinaK";
-export { default as Alex } from "./Alex/Alex";
-export { default as Varvara } from "./Varvara/Varvara";
+export { default as Polina } from './Polina/Polina';
+export { default as PolinaK } from './PolinaK/PolinaK';
+export { default as Alex } from './Alex/Alex';
+export { default as Varvara } from './Varvara/Varvara';
+export { default as PolinaDecorators } from './Polina/PolinaDecorators';
+export { default as PolinaKDecorators } from './PolinaK/PolinaKDecorators';
+export { default as AlexDecorators } from './Alex/AlexDecorators';
+export { default as VarvaraDecorators } from './Varvara/VarvaraDecorators';
