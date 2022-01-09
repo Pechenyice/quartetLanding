4e2dcc5ed13c77a9dc69221495a366160fa9a0c0
pechenyice <german.iskhakov@mail.ru>
2022-01-09 20:33:35 +0300

    added screens enum data

--- types/enums.ts
+++ types/enums.ts
@@ -1,19 +1,21 @@
 export enum Instrument {
-  VIOLIN = 'Скрипка',
-  CELLO = 'Виолончель',
-  ALTO = 'Альт',
+	VIOLIN = "Скрипка",
+	CELLO = "Виолончель",
+	ALTO = "Альт",
 }
 
 export enum Floor {
-  'Clio quartet',
-  'О нас',
-  'Партнеры',
-  'Выступления',
-  'Наши медиа',
+	"Clio quartet",
+	"О нас",
+	"Партнеры",
+	"Выступления",
+	"Наши медиа",
 }
 
 export enum Navigation {
-  'Clio quartet' = 0,
-  'Наша команда' = 1,
-  'О нас' = 2,
+	"Clio quartet" = 0,
+	"Наша команда" = 1,
+	"О нас" = 2,
+	"Выступления" = 3,
+	"Архив" = 4,
 }
