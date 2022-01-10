cde7a52e4324cffa7a53ad0cf1f90dcbf8c73400
Pechenyice <german.iskhakov@mail.ru>
2022-01-11 00:11:44 +0300

    renamed new version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,99 @@
-import React, { useRef } from "react";
-import { useEffect, useState } from "react";
-import styles from "@Styles/Home.module.css";
-import * as members from "@Members";
-import {
-	Featuring,
-	FloorsDisplay,
-	Footer,
-	Gallery,
-	Intro,
-	KvartetDisplay,
-	Media,
-} from "@Components";
-import { KvartetMember } from "@Types";
+import React from 'react';
+import { useEffect, useState } from 'react';
+import styles from '@Styles/Alternate/Alternate.module.css';
+import * as members from '@Members';
+import { Navigation, VersionManager } from '@Components';
+import { KvartetMember } from '@Types';
+import { IScreenManager } from '@Types/interfaces';
+import { getScreen, screens } from '@Screens';
+import { Key } from 'ts-keycode-enum';
 
 const Home = ({ kvartet }: { kvartet: string }) => {
-	let [_kvartet, setKvartet] = useState([] as KvartetMember[]);
+  let [_kvartet, setKvartet] = useState([] as KvartetMember[]);
 
-	let [floor, setFloor] = useState(1);
+  useEffect(() => {
+    setKvartet(JSON.parse(kvartet));
+  }, [kvartet]);
 
-	let app = useRef(null as unknown as HTMLElement);
+  let [screenManager, updateScreenManager] = useState({
+    activeScreen: 0,
+    nextScreen: null,
+    isSwapping: false,
+    animationDuration: 2000,
+    sibling: null,
+  } as IScreenManager);
 
-	useEffect(() => {
-		setKvartet(JSON.parse(kvartet));
-	}, [kvartet]);
+  function commitScreen(index: number) {
+    clearTimeout(screenManager.sibling as unknown as number);
+    updateScreenManager({
+      ...screenManager,
+      activeScreen: index,
+      nextScreen: null,
+      isSwapping: false,
+      sibling: null,
+    });
+  }
 
-	useEffect(() => {
-		let sections = Array.from(app.current?.children).filter(
-			(s) => s.localName === "section"
-		);
-		sections.reverse();
-		document.addEventListener("scroll", () => {
-			for (let section in sections) {
-				if (
-					sections[section].getBoundingClientRect().top <=
-					screen.height / 2
-				) {
-					setFloor(sections.length - +section);
-					return;
-				}
-			}
-		});
-	}, []);
+  function setNewScreen(index: number) {
+    if (screenManager.isSwapping || index < 0 || index >= screens.length) return;
+    updateScreenManager({
+      ...screenManager,
+      nextScreen: index,
+      isSwapping: true,
+      sibling: setTimeout(() => {
+        commitScreen(index);
+      }, screenManager.animationDuration),
+    });
+  }
 
-	return (
-		<section className={styles.app} ref={app}>
-			<FloorsDisplay floor={floor} />
-			<Intro kvartet={_kvartet} />
-			<KvartetDisplay kvartet={_kvartet} />
-			<Featuring />
-			<Gallery />
-			<Media />
-			<Footer kvartet={_kvartet} />
-		</section>
-	);
+  useEffect(() => {
+    document.addEventListener('wheel', wheelManager);
+    document.addEventListener('keydown', keyManager);
+
+    return () => {
+      document.removeEventListener('wheel', wheelManager);
+      document.removeEventListener('keydown', keyManager);
+    };
+  }, [screenManager.activeScreen]);
+
+  function keyManager(e: KeyboardEvent) {
+    if (e.which === Key.DownArrow) {
+      setNewScreen(screenManager.activeScreen + 1);
+    } else if (e.which === Key.UpArrow) {
+      setNewScreen(screenManager.activeScreen - 1);
+    }
+  }
+
+  function wheelManager(e: WheelEvent) {
+    if (e.deltaY > 0) {
+      setNewScreen(screenManager.activeScreen + 1);
+    } else {
+      setNewScreen(screenManager.activeScreen - 1);
+    }
+  }
+
+  function clickManager(index: number) {
+    setNewScreen(index);
+  }
+
+  let ActiveScreen = getScreen(screenManager.activeScreen);
+
+  return (
+    <section className={styles.app}>
+      <ActiveScreen.component isActive={!screenManager.isSwapping} />
+      <Navigation activePoint={ActiveScreen.navigation} clickManager={clickManager} />
+      <VersionManager />
+    </section>
+  );
 };
 
 export function getStaticProps(): { props: { kvartet: string } } {
-	let serializedMembers = KvartetMember.serializeMembers([
-		...Object.values(members),
-	]);
-	return {
-		props: {
-			kvartet: JSON.stringify(serializedMembers),
-		},
-	};
+  let serializedMembers = KvartetMember.serializeMembers([...Object.values(members)]);
+  return {
+    props: {
+      kvartet: JSON.stringify(serializedMembers),
+    },
+  };
 }
 
 export default Home;
