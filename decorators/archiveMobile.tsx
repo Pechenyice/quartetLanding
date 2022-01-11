851435644dd18a43f4577f9981f2722c24dd213e
Pechenyice <german.iskhakov@mail.ru>
2022-01-11 03:29:23 +0300

    added mobile decorators

---
+++ decorators/archiveMobile.tsx
@@ -0,0 +1,36 @@
+import { IDecorator } from '@Types/interfaces';
+
+const decorators: IDecorator[] = [
+  {
+    position: {
+      top: 120,
+      right: 100,
+    },
+    width: 506,
+    height: 506,
+    color: '#6D5A8D',
+    opacity: 0.4,
+  },
+  {
+    position: {
+      bottom: 100,
+      left: 146,
+    },
+    width: 300,
+    height: 300,
+    color: '#F28A4C',
+    opacity: 0.4,
+  },
+  {
+    position: {
+      bottom: 150,
+      left: 290,
+    },
+    width: 450,
+    height: 450,
+    color: '#8D5F5A',
+    opacity: 0.4,
+  },
+];
+
+export default decorators;
