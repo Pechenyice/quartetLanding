ce5172aa85f59b2bef4a4f394d1bc7d507940f41
Pechenyice <german.iskhakov@mail.ru>
2022-01-11 03:26:00 +0300

    added utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { ICascadeStyles } from '@Types/interfaces';
+
 function combineClasses(...classes: string[]): string {
   return classes.join(' ');
 }
@@ -6,4 +8,17 @@ function withAssetPrefix(url: string) {
   return process.env.NODE_ENV.toLowerCase() === 'production' ? `/quartetLanding/${url}` : `/${url}`;
 }
 
-export { combineClasses, withAssetPrefix };
+function createCascade(isMobile: boolean, styles: ICascadeStyles, mobileStyles: ICascadeStyles) {
+  let cascade: ICascadeStyles = {};
+  if (isMobile) {
+    cascade = { ...styles };
+    for (let [key, moduled] of Object.entries(mobileStyles)) {
+      if (cascade[key]) cascade[key] = combineClasses(cascade[key], moduled);
+    }
+  } else {
+    cascade = { ...styles };
+  }
+  return cascade;
+}
+
+export { combineClasses, withAssetPrefix, createCascade };
