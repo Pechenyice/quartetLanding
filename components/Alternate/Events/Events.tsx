3152b6fe2ff53802c65a76ee0e2ff5ec1fd1638f
Pechenyice <german.iskhakov@mail.ru>
2022-01-12 00:04:16 +0300

    added mobile support

--- components/Alternate/Events/Events.tsx
+++ components/Alternate/Events/Events.tsx
@@ -74,61 +74,65 @@ const Events = ({ isActive, isMobile, options }: IScreenWithOptionsProps) => {
       <div className={cascade.animator}>
         <div className={cascade.block}>
           <h3 className={cascade.blockName}>Следующее выступление</h3>
-          <div className={cascade.content}>
-            <div className={cascade.info}>
-              <div
-                className={cascade.elem}
-                style={{ '--delay': '0s', '--translation': '50px' } as CSSProperties}
-              >
-                <h4 className={cascade.elemName}>Название программы</h4>
-                <p className={cascade.elemContent}>{Program.name}</p>
+          <div className={cascade.contentFlexer}>
+            <div className={cascade.content}>
+              <div className={cascade.info}>
+                <div
+                  className={cascade.elem}
+                  style={{ '--delay': '0s', '--translation': '50px' } as CSSProperties}
+                >
+                  <h4 className={cascade.elemName}>Название программы</h4>
+                  <p className={cascade.elemContent}>{Program.name}</p>
+                </div>
+                <div
+                  className={cascade.elem}
+                  style={{ '--delay': '0.2s', '--translation': '-50px' } as CSSProperties}
+                >
+                  <h4 className={cascade.elemName}>Продолжительность</h4>
+                  <p className={cascade.elemContent}>{Program.duration}</p>
+                </div>
+                <div
+                  className={cascade.elem}
+                  style={{ '--delay': '0.4s', '--translation': '50px' } as CSSProperties}
+                >
+                  <h4 className={cascade.elemName}>В программе</h4>
+                  <p className={cascade.elemContent}>{Program.content.join(', ')}</p>
+                </div>
               </div>
-              <div
-                className={cascade.elem}
-                style={{ '--delay': '0.2s', '--translation': '-50px' } as CSSProperties}
-              >
-                <h4 className={cascade.elemName}>Продолжительность</h4>
-                <p className={cascade.elemContent}>{Program.duration}</p>
+              <div className={cascade.image}>
+                <Image src={poster} layout="fill" objectFit="contain" />
               </div>
-              <div
-                className={cascade.elem}
-                style={{ '--delay': '0.4s', '--translation': '50px' } as CSSProperties}
-              >
-                <h4 className={cascade.elemName}>В программе</h4>
-                <p className={cascade.elemContent}>{Program.content.join(', ')}</p>
-              </div>
-            </div>
-            <div className={cascade.image}>
-              <Image src={poster} layout="fill" objectFit="contain" />
-            </div>
-            <div className={cascade.info}>
-              <div
-                className={cascade.elem}
-                style={{ '--delay': '0.6s', '--translation': '-50px' } as CSSProperties}
-              >
-                <h4 className={cascade.elemName}>Мероприятие</h4>
-                <p className={cascade.elemContent}>{Information.name}</p>
-              </div>
-              <div
-                className={cascade.elem}
-                style={{ '--delay': '0.8s', '--translation': '50px' } as CSSProperties}
-              >
-                <h4 className={cascade.elemName}>Место проведения</h4>
-                <p className={cascade.elemContent}>{Information.location}</p>
-              </div>
-              <div
-                className={cascade.elem}
-                style={{ '--delay': '1s', '--translation': '-50px' } as CSSProperties}
-              >
-                <h4 className={cascade.elemName}>Дата</h4>
-                <p className={cascade.elemContent}>{Information.date}</p>
-              </div>
-              <div
-                className={cascade.elem}
-                style={{ '--delay': '1.2s', '--translation': '50px' } as CSSProperties}
-              >
-                <h4 className={cascade.elemName}>Стоимость билета</h4>
-                <p className={cascade.elemContent}>От {Information.price} ₽</p>
+              <div className={cascade.info}>
+                <div
+                  className={cascade.elem}
+                  style={{ '--delay': '0.6s', '--translation': '-50px' } as CSSProperties}
+                >
+                  <h4 className={cascade.elemName}>Мероприятие</h4>
+                  <p className={cascade.elemContent}>{Information.name}</p>
+                </div>
+                <div
+                  className={cascade.elem}
+                  style={{ '--delay': '0.8s', '--translation': '50px' } as CSSProperties}
+                >
+                  <h4 className={cascade.elemName}>Место проведения</h4>
+                  <p className={cascade.elemContent}>{Information.location}</p>
+                </div>
+                <div className={cascade.smallSleeper}>
+                  <div
+                    className={cascade.elem}
+                    style={{ '--delay': '1s', '--translation': '-50px' } as CSSProperties}
+                  >
+                    <h4 className={cascade.elemName}>Дата</h4>
+                    <p className={cascade.elemContent}>{Information.date}</p>
+                  </div>
+                  <div
+                    className={cascade.elem}
+                    style={{ '--delay': '1.2s', '--translation': '50px' } as CSSProperties}
+                  >
+                    <h4 className={cascade.elemName}>Стоимость билета</h4>
+                    <p className={cascade.elemContent}>От {Information.price} ₽</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
