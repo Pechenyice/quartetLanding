41b0b5bc75ccbbfa8e824911c21ad22ce1cf9264
Pechenyice <german.iskhakov@mail.ru>
2022-01-11 03:29:47 +0300

    updated index

--- decorators/index.ts
+++ decorators/index.ts
@@ -1,3 +1,6 @@
 export { default as quotesDecorators } from './quotes';
 export { default as eventsDecorators } from './events';
 export { default as archiveDecorators } from './archive';
+export { default as quotesMobileDecorators } from './quotesMobile';
+export { default as eventsMobileDecorators } from './eventsMobile';
+export { default as archiveMobileDecorators } from './archiveMobile';
