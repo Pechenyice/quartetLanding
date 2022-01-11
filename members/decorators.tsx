936fad0baddd2c9a7c71714e5deecfb17009b297
Pechenyice <german.iskhakov@mail.ru>
2022-01-11 03:28:50 +0300

    updated index

--- members/decorators.tsx
+++ members/decorators.tsx
@@ -2,3 +2,7 @@ export { default as PolinaDecorators } from './Polina/PolinaDecorators';
 export { default as PolinaKDecorators } from './PolinaK/PolinaKDecorators';
 export { default as AlexDecorators } from './Alex/AlexDecorators';
 export { default as VarvaraDecorators } from './Varvara/VarvaraDecorators';
+export { default as PolinaMobileDecorators } from './Polina/PolinaMobileDecorators';
+export { default as PolinaKMobileDecorators } from './PolinaK/PolinaKMobileDecorators';
+export { default as AlexMobileDecorators } from './Alex/AlexMobileDecorators';
+export { default as VarvaraMobileDecorators } from './Varvara/VarvaraMobileDecorators';
