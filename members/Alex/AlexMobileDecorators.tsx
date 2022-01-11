3ed136d99b74b1898df84922b1c37964ac828241
Pechenyice <german.iskhakov@mail.ru>
2022-01-11 03:29:03 +0300

    added mobile decorators

---
+++ members/Alex/AlexMobileDecorators.tsx
@@ -0,0 +1,26 @@
+import { IDecorator } from '@Types/interfaces';
+
+const circles: IDecorator[] = [
+  {
+    position: {
+      right: 150,
+      bottom: 110,
+    },
+    width: 460,
+    height: 460,
+    color: '#8D5F5A',
+    opacity: 0.4,
+  },
+  {
+    position: {
+      bottom: 326,
+      right: 496,
+    },
+    width: 312,
+    height: 312,
+    color: '#F28A4C',
+    opacity: 0.4,
+  },
+];
+
+export default circles;
