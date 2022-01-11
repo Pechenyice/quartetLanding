0c5ab3096b856c9b21330d8ea638db568f2bd11b
Pechenyice <german.iskhakov@mail.ru>
2022-01-11 03:29:32 +0300

    added mobile decorators

---
+++ decorators/quotesMobile.ts
@@ -0,0 +1,26 @@
+import { IDecorator } from '@Types/interfaces';
+
+const decorators: IDecorator[] = [
+  {
+    position: {
+      top: 60,
+      right: 300,
+    },
+    width: 400,
+    height: 400,
+    color: '#E3A987',
+    opacity: 0.4,
+  },
+  {
+    position: {
+      top: 170,
+      right: 555,
+    },
+    width: 550,
+    height: 550,
+    color: '#7B6866',
+    opacity: 0.4,
+  },
+];
+
+export default decorators;
