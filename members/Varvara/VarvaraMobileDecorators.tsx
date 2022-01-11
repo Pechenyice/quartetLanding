a8441f0e8b2a2be795d9efbde8d17b291f9d5c39
Pechenyice <german.iskhakov@mail.ru>
2022-01-11 03:29:12 +0300

    added mobile decorators

---
+++ members/Varvara/VarvaraMobileDecorators.tsx
@@ -0,0 +1,26 @@
+import { IDecorator } from '@Types/interfaces';
+
+const circles: IDecorator[] = [
+  {
+    position: {
+      bottom: 190,
+      right: 305,
+    },
+    width: 312,
+    height: 312,
+    color: '#1D7C0D',
+    opacity: 0.4,
+  },
+  {
+    position: {
+      right: 505,
+      bottom: 212,
+    },
+    width: 461,
+    height: 461,
+    color: '#C46129',
+    opacity: 0.4,
+  },
+];
+
+export default circles;
