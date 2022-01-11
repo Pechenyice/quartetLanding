993f41ace6289324f72eb5c53450b027e26a090b
Pechenyice <german.iskhakov@mail.ru>
2022-01-11 03:29:06 +0300

    added mobile decorators

---
+++ members/Polina/PolinaMobileDecorators.ts
@@ -0,0 +1,26 @@
+import { IDecorator } from '@Types/interfaces';
+
+const circles: IDecorator[] = [
+  {
+    position: {
+      bottom: 190,
+      left: 305,
+    },
+    width: 312,
+    height: 312,
+    color: '#522EBA',
+    opacity: 0.4,
+  },
+  {
+    position: {
+      left: 505,
+      bottom: 212,
+    },
+    width: 460,
+    height: 460,
+    color: '#CD794A',
+    opacity: 0.4,
+  },
+];
+
+export default circles;
