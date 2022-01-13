f83a9cd12234fb2aa2f37e2c530dfe32c2f6fe92
Pechenyice <german.iskhakov@mail.ru>
2022-01-13 03:30:03 +0300

    fixed implementation error

--- types/index.ts
+++ types/index.ts
@@ -7,12 +7,14 @@ export class KvartetMember implements IKvartetMember {
   };
 
   readonly name: IKvartetMember['name'];
+  readonly screenName: IKvartetMember['screenName'];
   readonly preview: IKvartetMember['preview'];
   readonly instrument: IKvartetMember['instrument'];
   readonly about: IKvartetMember['about'];
 
   constructor(member: IKvartetMember) {
     this.name = member.name;
+    this.screenName = member.screenName;
     this.preview = member.preview;
     this.instrument = member.instrument;
     this.about = member.about;
